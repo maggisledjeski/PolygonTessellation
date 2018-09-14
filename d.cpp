73cbc4e33d412e58bbafffd7f262efcfb42b5f85
Maggi Sledjeski <sledjeski_ma@gsc_405_04.mercer.edu>
2018-09-14 13:59:34 -0400

    got the intersection point woo

--- d.cpp
+++ d.cpp
@@ -11,9 +11,23 @@ struct linseg {
     vertex one;
     vertex two;
     //build line equation
-    //    //draw line equation
+    //draw line equation
 };
-    //
+    
+void cp(linseg a, linseg b)
+{
+    vertex v;
+    v.x = 3;
+}
+
+float Determinant2(vertex a, vertex b)
+{
+    //printf("a.x: %f    a.y: %f    b.x: %f    b.y: %f\n",a.x,a.y,b.x,b.y);
+    float d = (a.x * b.y) - (a.y * b.x);
+    //printf("%f\n",d);
+    return d;
+}
+
 void linIntersect(linseg a, linseg b)
 {
     float P1 = b.one.x - a.one.x;
@@ -21,6 +35,9 @@ void linIntersect(linseg a, linseg b)
     float P3 = b.one.y - a.one.y;
     float P4 = -(b.two.y - b.one.y);
     
+    //float c1 = Determinant2(a.one,a.two);
+    //float c2 = Determinant2(b.one,b.two);
+
     float P5 = a.two.x - a.one.x;
     float P6 = -(b.two.x - b.one.x);
     float P7 = a.two.y - a.one.y;
@@ -34,10 +51,19 @@ void linIntersect(linseg a, linseg b)
     float d1 = (P1*P4) - (P2*P3);
     float d2 = (P5*P8) - (P6*P7);
     float d3 = (P9*P12) - (P10*P11);
+    
+    //printf("d1: %f    d2: %f   d3: %f\n",d1,d2,d3);
 
+    //float u1 = c1/d2;
+    //float u2 = c2/d2;
     float ua = d1/d2;
     float ub = d3/d2;
     printf("ua: %f\nub: %f\n",ua,ub);
+    //printf("u1: %f\nu2: %f\n",u1,u2);
+    float x = a.one.x + ua*(a.two.x - a.one.x);
+    float y = a.one.y + ua*(a.two.y - a.one.y);
+    printf("x = %f\ny = %f\n",x,y);
+
 }
 
 int main(int argc, char** argv)
